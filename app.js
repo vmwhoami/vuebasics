557065aef0e5d0e81747615a46a89c5b1c2ca5c3
Vitalie Melnic <vmwhoami@gmail.com>
2021-07-24 18:57:35 +0300

    Add array of objects

--- app.js
+++ app.js
@@ -1,31 +1,29 @@
-const images = [
-  'https://www.careandliving.com/wp-content/uploads/2017/10/six-pack-men6.jpg',
-  'https://i.pinimg.com/originals/3e/07/65/3e0765c8a07d80d25edd3835f8e269f3.jpg',
-  'https://i.pinimg.com/originals/6a/1e/ca/6a1eca2ad41d91c7ea26f6748b6de71b.jpg',
-  'https://lol54.ru/wp-content/uploads/2019/04/083-4.jpeg',
+const persons = [
+  { gender: "male", firstName: "Alex", lastName: "Wong", img: 'https://www.careandliving.com/wp-content/uploads/2017/10/six-pack-men6.jpg' },
+  { gender: "male", firstName: "Joe", lastName: "Barbos", img: 'https://i.pinimg.com/originals/3e/07/65/3e0765c8a07d80d25edd3835f8e269f3.jpg' },
+  { gender: "male", firstName: "Hui", lastName: "EgoZnaet", img: 'https://i.pinimg.com/originals/6a/1e/ca/6a1eca2ad41d91c7ea26f6748b6de71b.jpg' },
+  { gender: "female", firstName: "Soe", lastName: "Bitch", img: 'https://lol54.ru/wp-content/uploads/2019/04/083-4.jpeg' },
 ]
-
 const app = Vue.createApp({
   data() {
     return {
       index: 0,
-      isMale: true,
-      img: 'img',
-      firstName: 'Adam',
-      lastName: 'Melnic',
+      isMale: true
     }
   },
   methods: {
-    changeIndx() {
-      this.index += 1
+    setIndex() {
+      if (persons[this.index] === null || persons[this.index] === undefined) {
+        return this.index = 0
+      }
+      return this.index += 1
+    },
+    setName() {
+      return `${this.firstName} ${this.lastName} `
     },
     setPicture() {
-      if (images[this.index] === null || images[this.index] === undefined) {
-        this.index = 0
-      }
-      return images[this.index]
+      return persons[this.index].img
     }
-
   }
 })
 
